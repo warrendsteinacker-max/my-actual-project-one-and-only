10d9a72ae22d2ed3c472606f57a8fda0e663e0fc
Warren <warrendsteinacker@gmail.com>
2026-01-09 18:08:39 -0500

    fixed my routing problem

--- src/pages/Auth/RegisterPage.jsx
+++ src/pages/Auth/RegisterPage.jsx
@@ -1,9 +1,9 @@
-// import React from 'react'
+import React from 'react'
 
-// const RegisterPage = () => {
-//   return (
-//     <div>RegisterPage</div>
-//   )
-// }
+const RegisterPage = () => {
+  return (
+    <div>RegisterPage</div>
+  )
+}
 
-// export default RegisterPage
+export default RegisterPage
