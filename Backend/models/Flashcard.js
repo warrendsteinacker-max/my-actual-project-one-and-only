f47b855bfff596fb166f34f7fb4c8f0db008dce6
Warren <warrendsteinacker@gmail.com>
2026-01-11 09:22:03 -0500

    think I made proper middle ware

--- Backend/models/Flashcard.js
+++ Backend/models/Flashcard.js
@@ -1,7 +1,19 @@
 
 
-const login = async(req, res, next) => {
+const login = async(error, req, res, next) => {
     const {email, pas} = req.body
 
     const ismatch = await Users.findOne({email})
+
+    if(ismatch){
+        res.status(400).json({error: error.message})
+    }
+
+    const isuser = await bcrypt.compaire(pas, this.password)
+
+    if(!isuser){
+        res.status(400).json({error: error.message})  
+    }
+
+    next()
 }
